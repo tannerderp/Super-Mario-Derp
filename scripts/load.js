51637c45b3b7e26c2a7a955e1cc4314f9b02d463
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2019-06-03 18:00:48 -0600

    loading scripts

---
+++ scripts/load.js
@@ -0,0 +1,36 @@
+var scripts;
+var load = {
+        amount: 0,
+        needed: 0
+};
+$.getJSON("/scripts/scripts.json", function(jsonData){
+    scripts = jsonData.scripts;
+
+    for(var i in scripts){
+        for(var j = 0; j < scripts[i].length; j ++){
+            load.needed++;
+            loadScript("/scripts/"+i+"/"+scripts[i][j], function(){
+                load.amount ++;
+                if(load.amount >= load.needed){
+                    new p5();
+                }
+            });
+        }
+    }
+});
+function loadScript(url, callback) {
+    // Adding the script tag to the head as suggested before
+    var body = document.querySelector("body");
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.charset = 'utf-8';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    body.appendChild(script);
+};
---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+function setup(){
+    createCanvas(1600, 900);
+}
+function draw(){
+    background(0, 0, 0);
+}
